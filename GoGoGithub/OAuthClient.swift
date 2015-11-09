dc1e8069512e9413f33b75156ffeee361f73890d
heidiyee <heidi.n.yee@gmail.com>
2015-11-09 13:39:17 -0800

    First commit

---
+++ GoGoGithub/OAuthClient.swift
@@ -0,0 +1,21 @@
+//
+//  OAuthClient.swift
+//  GoGoGithub
+//
+//  Created by Heidi Yee on 11/9/15.
+//  Copyright © 2015 Heidi Yee. All rights reserved.
+//
+
+import UIKit
+
+let kClientId = "d325e72fb3250a427806"
+let kClientSecret = "98c8c84c3c978c39977a5fc877e5ff0a2365de16"
+
+
+class OAuthClient {
+    
+    func requestGithubAccess() {
+        
+    }
+    
+}
